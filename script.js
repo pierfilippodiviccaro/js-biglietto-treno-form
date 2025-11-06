85ef3ab867dc2213085005f1c16f44525a64e4f8
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-06 18:21:47 +0100

    feat: finalizzato l'esercizio

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const kilometers = document.getElementById("kilometri")
 const bottoneSend = document.getElementById("button-submit")
 const bottoneRedo = document.getElementById("button-redo")
 const anniDivita = document.getElementById("quanto")
-let sconto = 0 
+let totScontato = 0
+const prezzo = document.getElementById("costo-biglietto")
+const numCarrozza = document.getElementById("num-carrozza")
+const codiceCp = document.getElementById("codice-CP") 
 // console.log(format,age, kilometers, bottoneSend,bottoneRedo);
 // aggiunta dell'evento di submit
 format.addEventListener("submit", function(event){
@@ -21,9 +24,14 @@ format.addEventListener("submit", function(event){
     else if (anniDivita >= 65) {
         sconto = (soldi * 40 / 100)
     }
-    soldi = soldi - sconto
-
+    sconto = soldi - sconto
+const soldoni = Math.floor(Math.random()*9000) +1 ;
     console.log(soldi);
+    codiceCp.innerHTML= soldoni 
+ const carrozzaAssegno = Math.floor(Math.random()*9) +1 ;
+    numCarrozza.innerHTML = carrozzaAssegno
+    prezzo.innerHTML=soldi
+    
 });
 
 
